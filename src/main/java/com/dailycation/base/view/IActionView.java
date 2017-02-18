5bd2a4d3657607f82a9fcf9f03cdbc7aa9c4c311
hoffer <hoffergg@gmail.com>
2017-02-18 22:54:50 +0800

    接口实现

---
+++ src/main/java/com/dailycation/base/view/IActionView.java
@@ -0,0 +1,13 @@
+package com.dailycation.base.view;
+
+/**
+ * Created by hoffer on 17/2/18.
+ */
+
+public interface IActionView extends IBaseView {
+
+    /**
+     * action success
+     */
+    void onActionSuccess();
+}
