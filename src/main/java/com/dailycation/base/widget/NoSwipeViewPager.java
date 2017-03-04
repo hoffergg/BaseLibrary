a24d1886222c107cfcf7113938d54134c49cf955
hoffer-XCBD <hoffergg@gmail.com>
2017-03-04 13:09:28 +0800

    add no swipe viewpager

---
+++ src/main/java/com/dailycation/base/widget/NoSwipeViewPager.java
@@ -0,0 +1,25 @@
+package com.dailycation.base.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by hehu on 17-3-4.
+ */
+
+public class NoSwipeViewPager extends ViewPager {
+    public NoSwipeViewPager(Context context) {
+        super(context);
+    }
+
+    public NoSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return true;
+    }
+}
