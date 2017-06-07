939302b7ed5e35e2f826b7c8e89acf6e61ba68c5
hoffer-XCBD <hoffergg@gmail.com>
2017-06-07 20:57:50 +0800

    init

---
+++ src/main/java/com/dailycation/base/presenter/IBaseResidentialPresenter.java
@@ -0,0 +1,58 @@
+package com.dailycation.base.presenter;
+
+import com.dailycation.base.model.IUser;
+
+/**
+ * Created by hehu on 17-6-7.
+ */
+
+public interface IBaseResidentialPresenter extends IBasePresenter2{
+
+    /**
+     * 登录
+     * @param userName 用户名
+     * @param password 密码
+     * @param verifyCode 验证码
+     */
+    void login(String userName,String password,String verifyCode);
+
+    /**
+     * 登录成功
+     * @param user 登录成功返回的用户
+     */
+    void onLoginSuccess(IUser user);
+
+    /**
+     * 检查输入
+     * @param userName
+     * @param password
+     * @param verifyCode
+     * @return
+     */
+    boolean checkLoginInput(String userName,String password,String verifyCode);
+
+    /**
+     * Go register
+     * @param userName
+     * @param password
+     * @param rePassword
+     * @param verifyCode
+     */
+    void register(String userName,String password,String rePassword,String verifyCode);
+
+    /**
+     * check all the input
+     * @param userName
+     * @param password
+     * @param rePassword
+     * @param verifyCode
+     * @return
+     */
+    boolean checkRegisterInput(String userName,String password,String rePassword,String verifyCode);
+
+    /**
+     * request verify code for a phone
+     * @param phone
+     */
+    void requestVerifyCode(String phone);
+}
---
+++ src/main/java/com/dailycation/base/view/IBaseResidentialView.java
@@ -0,0 +1,8 @@
+package com.dailycation.base.view;
+
+/**
+ * Created by hehu on 17-6-7.
+ */
+
+public interface IBaseResidentialView extends IBaseView{
+}
