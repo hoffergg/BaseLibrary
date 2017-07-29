37d8f90c660bc021e829af9a0318259bfea6233b
hoffer <hoffergg@gmail.com>
2017-07-29 15:13:18 +0800

    friend detail

---
+++ src/main/java/com/dailycation/base/util/RecyclerViewUtil.java
@@ -0,0 +1,25 @@
+package com.dailycation.base.util;
+
+import android.support.v7.widget.LinearLayoutManager;
+
+/**
+ * RecyclerView utils
+ * Created by hoffer on 17/7/29.
+ */
+
+public class RecyclerViewUtil {
+
+    /**
+     * check is linear RecyclerView scrolled to bottom.
+     * @param linearLayoutManager
+     * @return
+     */
+    public static boolean isLinearScrolledBottom(LinearLayoutManager linearLayoutManager){
+        int pastVisibleItems, visibleItemCount, totalItemCount;
+        visibleItemCount = linearLayoutManager.getChildCount();
+        totalItemCount = linearLayoutManager.getItemCount();
+        pastVisibleItems = linearLayoutManager.findFirstVisibleItemPosition();
+
+        return (visibleItemCount + pastVisibleItems) >= totalItemCount;
+    }
+}
