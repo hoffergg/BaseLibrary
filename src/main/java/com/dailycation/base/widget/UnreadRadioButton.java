ae435ace80da79130ce09bff94121175877f90fc
hoffer <hoffergg@gmail.com>
2018-10-22 13:43:28 +0800

    main tab

---
+++ src/main/java/com/dailycation/base/widget/UnreadRadioButton.java
@@ -0,0 +1,55 @@
+package com.dailycation.base.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+
+public class UnreadRadioButton extends android.support.v7.widget.AppCompatRadioButton {
+    public UnreadRadioButton(Context context) {
+        super(context);
+    }
+
+    public UnreadRadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UnreadRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * unread count.
+     */
+    int unreadCount = 0;
+
+    /**
+     * default red dot radius, in pixels.
+     */
+    int radius = 10;
+
+    /**
+     * default red color.
+     */
+    String redColor = "#fc291c";
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if(unreadCount>0)
+            drawRedDot(canvas);
+    }
+
+    public void setUnreadCount(int unreadCount) {
+        this.unreadCount = unreadCount;
+        invalidate();
+    }
+
+    public void drawRedDot(Canvas canvas){
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor(redColor));
+        canvas.drawCircle(getWidth()-radius,radius,radius,paint);
+    }
+}
